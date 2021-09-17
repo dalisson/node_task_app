733e7945cfc4832b0db3140d5345c1d65848e759
dalissonfigueiredo <dalissonfigueiredo@gmail.com>
2021-09-17 19:32:35 -0300

    removed email sending on creation

--- src/routers/user.js
+++ src/routers/user.js
@@ -28,8 +28,8 @@ router.post('/users', async (req, res)=>{
     const user = new User(req.body)
     try{
         //sendWelcomeMail(req.body.email, req.body.name)
-        const token = await user.generateAuthToken()
-        await user.save()
+        //const token = await user.generateAuthToken()
+        //await user.save()
         res.status(201).send({user,token})
     }catch(e){
         res.status(400).send(e)
