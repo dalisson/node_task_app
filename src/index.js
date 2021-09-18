cf9cc918181b4ea80263fe0a215bde5dc88ed3b8
dalissonfigueiredo <dalissonfigueiredo@gmail.com>
2021-09-18 13:24:23 -0300

    refactoring index

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
-const express = require('express')
-require('./db/mongoose')
-
-const usrRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-
-const app = express()
+const app = require('./app')
 const port = process.env.PORT
-
-// app.use((req, res, next)=>{
-
-//     res.status(503).send("server under maintencance")
-// })
-
-app.use(express.json())
-app.use(taskRouter)
-app.use(usrRouter)
 
 app.listen(port, ()=>{
     console.log(`apps listening on port ${port}`)
